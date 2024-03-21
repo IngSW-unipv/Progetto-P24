39fdf86dc4c37c6f1faa3f286d99621533b899f5
RiccardoCorsaro02 <151643548+RiccardoCorsaro02@users.noreply.github.com>
2024-03-21 11:49:03 +0100

    Delete NameNotFound directory

--- NameNotFound/src/NextFit/Dietista.java
+++
@@ -1,8 +0,0 @@
-package NextFit;
-
-public class Dietista extends Dipendente {
-	public Dietista(String nome, String cognome, String mail, String password, int età, double stipendio, String tipo) {
-		super(nome, cognome, mail, password, età, stipendio, tipo);
-	}
-
-}
