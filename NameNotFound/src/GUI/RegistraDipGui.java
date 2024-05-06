7e3a669626f2de6de7abd4b1391f276a98dbdf1b
RiccardoCorsaro02 <151643548+RiccardoCorsaro02@users.noreply.github.com>
2024-05-06 12:14:35 +0200

    Add files via upload

--- NameNotFound/src/GUI/RegistraDipGui.java
+++ NameNotFound/src/GUI/RegistraDipGui.java
@@ -40,25 +40,29 @@ public class RegistraDipGui extends JFrame {
 		panel.add(Box.createRigidArea(new Dimension(0, 20)));
 
 		nomeField = new JTextField();
-		nomeField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+
 		nomeField.setForeground(Color.white);
 		cognomeField = new JTextField();
-		cognomeField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+		
 		cognomeField.setForeground(Color.white);
 		mailField = new JTextField();
-		mailField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+
 		mailField.setForeground(Color.white);
 		etaField = new JTextField();
-		etaField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+
 		etaField.setForeground(Color.white);
-		tipoField = new JTextField();
-		tipoField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+		
+
+		String[] options = {"PersonalTrainer", "Corsista", "Dietista"};
+		JComboBox<String> tipoField = new JComboBox<>(options);
 		tipoField.setForeground(Color.white);
+		
+	
+		
 		passwordField = new JPasswordField();
-		passwordField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+
 		passwordField.setForeground(Color.white);
 		stipField = new JTextField();
-		stipField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
 		stipField.setForeground(Color.white);
 
 		Color CBUT = new Color(40, 40, 40);
@@ -83,6 +87,7 @@ public class RegistraDipGui extends JFrame {
 		stipField.setBackground(CBUT);
 
 		nome = new JLabel("Nome:");
+		nome.setMaximumSize(new Dimension(Integer.MAX_VALUE, 20));
 		nome.setForeground(Color.white);
 		cognome = new JLabel("Cognome:");
 		cognome.setForeground(Color.white);
@@ -97,34 +102,36 @@ public class RegistraDipGui extends JFrame {
 		stipendio = new JLabel("Stipendio:");
 		stipendio.setForeground(Color.white);
 
+		
+		
 		panel.add(nome);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(nomeField);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(cognome);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(cognomeField);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(mail);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(mailField);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(eta);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(etaField);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(tipo);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(tipoField);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(pw);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(passwordField);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(stipendio);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 		panel.add(stipField);
-		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(Box.createRigidArea(new Dimension(0, 10)));
 
 		panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
@@ -137,7 +144,7 @@ public class RegistraDipGui extends JFrame {
 
 		registraButton.setBackground(or);
 		registraButton.setFont(new Font("Arial", Font.BOLD, 13));
-		registraButton.setMaximumSize(new Dimension(100, 30));
+		registraButton.setMaximumSize(new Dimension(150, 30));
 		registraButton.setBorder(BorderFactory.createLineBorder(or, 6, false));
 
 		registraButton.addActionListener(new ActionListener() {
@@ -147,49 +154,49 @@ public class RegistraDipGui extends JFrame {
 				String nome = nomeField.getText();
 				String cognome = cognomeField.getText();
 				String mail = mailField.getText();
-				String tipo = tipoField.getText();
+				String tipo = (String) tipoField.getSelectedItem();
 				String password = new String(passwordField.getPassword());
 				int eta = Integer.parseInt(etaField.getText());
 				double stipendio = Double.parseDouble(stipField.getText());
 
 				Dipendente d = palestra.creaDipendente(nome, cognome, mail, password, eta, stipendio, tipo);
-				// palestra.registraDipendente(d);
-
+				//palestra.registraDipendente(d);
+				
 				JPanel buttonPanel = new JPanel();
 				buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
 				buttonPanel.setBackground(CBACK);
 				buttonPanel.add(registraButton);
 				buttonPanel.add(errorLabel);
 
-				if (palestra.registraDipendente(d) == true) {
-
-					buttonPanel.add(registraButton);
-					buttonPanel.remove(errorLabel);
-					panel.add(buttonPanel);
-					panel.revalidate();
-					panel.repaint();
-				}
-
-				/*
-				 * SwingUtilities.invokeLater(new Runnable() { public void run() { // new
-				 * LatoClienteGui(co, cliente, palestra); //new AbbonamentoGui(cliente,
-				 * proprietario, palestra); } }); }
-				 */ else {
-
-					panel.add(buttonPanel);
-
-					// Aggiorna il pannello
-					panel.revalidate();
-					panel.repaint();
-
-				}
-
+				
+				  if (palestra.registraDipendente(d) == true) {
+				  
+				  buttonPanel.add(registraButton); 
+				  buttonPanel.remove(errorLabel);
+				  panel.add(buttonPanel); 
+				  panel.revalidate(); 
+				  panel.repaint();}
+				  
+			/*	  SwingUtilities.invokeLater(new Runnable() { public void run() { // new
+				 LatoClienteGui(co, cliente, palestra); //new AbbonamentoGui(cliente,
+				 proprietario, palestra); } }); }*/ else {
+				  
+				 panel.add(buttonPanel);
+				// Aggiorna il pannello 
+				  panel.revalidate(); 
+				  panel.repaint();
+				
+				  }
+				 
 			}
-		});
+		}); 
+		
 		panel.add(registraButton);
 
 		getContentPane().add(panel, BorderLayout.CENTER);
 
+		
+		
 		setSize(480, 640); // Impostiamo le dimensioni della finestra
 		setLocationRelativeTo(null); // Posizioniamo la finestra al centro dello schermo
 		setVisible(true);
