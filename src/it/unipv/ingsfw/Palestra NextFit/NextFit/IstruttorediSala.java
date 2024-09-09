71b1a2467f1a8a28f59152095a0619e6489475c7
RiccardoCorsaro02 <151643548+RiccardoCorsaro02@users.noreply.github.com>
2024-09-09 11:08:25 +0200

    Delete src/it/unipv/ingsfw/Palestra NextFit directory

--- src/it/unipv/ingsfw/Palestra NextFit/NextFit/IstruttorediSala.java
+++
@@ -1,9 +0,0 @@
-package NextFit;
-
-public class IstruttorediSala extends Dipendente {
-	public IstruttorediSala(String nome, String cognome, String mail, String password, int età, double stipendio, String tipo) {
-		super(nome, cognome, mail, password, età, stipendio, tipo);
-
-	}
-
-}
